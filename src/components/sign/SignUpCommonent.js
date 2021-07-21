72c58263885d9d74f969b4596dcb7b380da32163
Sinminbeom <alsqja17@naver.com>
2021-07-21 17:59:32 +0900

    웹팩 디버깅 추가

--- src/components/sign/SignUpCommonent.js
+++ src/components/sign/SignUpCommonent.js
@@ -1,7 +1,7 @@
-import {Button, Form, Input} from "antd";
-import {LockOutlined, MailOutlined, UserOutlined} from "@ant-design/icons";
+import {Button, Form, Input, Upload, message} from "antd";
+import {LockOutlined, MailOutlined, UserOutlined, LoadingOutlined, PlusOutlined} from "@ant-design/icons";
 import {Link, Route} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Redirect} from "react-router";
 import { ServiceComponent } from '../service/ServiceComponent';
 
@@ -11,7 +11,8 @@ const SignUpCommonent = () => {
     const [isValid, setIsValid] = useState(false);
     const [error, setError] = useState("");
     const [isSuccess,setIsSuccess] = useState(false);
-
+    const [loading, setLoading] = useState(false);
+    
     useEffect(() => {
         if(isValid){
             form.setFields([{
@@ -23,41 +24,52 @@ const SignUpCommonent = () => {
 
     const CallBack = (result) => {
         if(result.result){
-
+            alert("회원가입 완료");
+            setIsSuccess(true);
+            this.props.history.push('/signin');
         }
         else{
-          alert(result.message);
+            alert(result.message);
+            setError(result.message);
+            setIsValid(true);
         }
-      }
+    }
 
     function onFinish(values) {
         setIsValid(false);
         setError("");
 
-        // if(!CommentValue){
-        //     return alert('댓글 내용을 입력해주세요.');
-        // }
-        console.log(values.email);
-        // var formData = new FormData();
+        console.log(values);
+        var formData = new FormData();
 
-        // formData.append('name',props.postSeq);
-        // formData.append('password',CommentValue);
-        // formData.append('email',NickName);
-
-        // ServiceComponent('http://49.168.71.214:8000/SignUpSave.php',formData,CallBack);
+        formData.append('UserName',values.name);
+        formData.append('Email',values.email);
+        formData.append('Password',values.password);
+        // formData.append('Image',)
+        ServiceComponent('http://49.168.71.214:8000/SignUpSave.php',formData,CallBack);
         
-        // addUser(values).then(response=>{
-        //     if(response.status===400){
-        //         setIsValid(true);
-        //         setError(response.message)
-        //     }
-        //     if(response.status===200){
-        //         alert("등록성공")
-        //         console.log("등록성공.");
-        //         setIsSuccess(true);
-        //     }
-        // });
     }
+    const getBase64 = (img, callback) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(img);
+    }
+    const handleChange = (info) => {
+        if (info.file.status === 'uploading') {
+          setLoading(true);
+          return;
+        }
+        if (info.file.status === 'done') {
+          // Get this url from response in real world.
+          getBase64(info.file.originFileObj, imageUrl =>
+            this.setState({
+              imageUrl,
+              loading: false,
+            }),
+          );
+        }
+    };
+
     return(
         <>
             <Form form={form} name={"normal_login"}
@@ -66,6 +78,19 @@ const SignUpCommonent = () => {
                   onFinish={onFinish}
                   xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}
             >
+                {/* <Form.Item>
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        beforeUpload={beforeUpload}
+                        onChange={this.handleChange}
+                    >
+                        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                    </Upload>
+                </Form.Item> */}
                 <Form.Item
                     name={"name"}
                     rules={
@@ -74,7 +99,7 @@ const SignUpCommonent = () => {
                             message: 'Please Input your Username'
                         }]
                     }>
-                    <Input prefix={<UserOutlined className={"site-form-item-icon"} placeholder={"Username"}/>}/>
+                    <Input prefix={<UserOutlined className={"site-form-item-icon"}/>} placeholder={"이름(아이디)"}/>
                 </Form.Item>
                 <Form.Item
                     name={"email"}
@@ -85,7 +110,7 @@ const SignUpCommonent = () => {
                             message: 'Please Input your Email'
                         }]
                     }>
-                    <Input prefix={<MailOutlined className={"site-form-item-icon"} placeholder={"Email"}/>}/>
+                    <Input prefix={<MailOutlined className={"site-form-item-icon"}/>} placeholder={"이메일"}/>
                 </Form.Item>
                 <Form.Item
                     name={"password"}
@@ -98,14 +123,14 @@ const SignUpCommonent = () => {
                     <Input
                         prefix={<LockOutlined className={"site-form-item-icon"}/>}
                         type={"password"}
-                        placeholder={"Password"}
+                        placeholder={"비밀번호"}
                     />
                 </Form.Item>
                 <Form.Item>
                     <Button type={"primary"} htmlType={"submit"} className={"login-form-button"}>
                         가입완료
                     </Button>
-                    Or <Link to={"/signin"}>로그인</Link>
+                     Or <Link to={"/signin"}>로그인</Link>
                 </Form.Item>
             </Form>
             <Route>
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {                                      // moduel export (옛날
         path: path.join(__dirname, '/dist'),            //__dirname : 현재 디렉토리, dist 폴더에 모든 컴파일된 하나의 번들파일을 넣을 예정
         filename: 'index_bundle.js'
     },
+    mode: 'development',
+    //devtool: 'inline-source-map',
     module: {                                           // javascript 모듈을 생성할 규칙을 지정 (node_module을 제외한.js 파일을 babel-loader로 불러와 모듈을 생성
         rules: [
             {
