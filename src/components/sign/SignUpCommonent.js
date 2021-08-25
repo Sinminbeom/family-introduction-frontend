7f820702344772b99b17ed8f31c2f35c52b723b5
Sinminbeom <alsqja17@naver.com>
2021-08-25 22:12:10 +0900

    회원가입 프로필 upload

--- src/components/sign/SignUpCommonent.js
+++ src/components/sign/SignUpCommonent.js
@@ -67,7 +67,17 @@ const SignUpCommonent = () => {
           );
         }
     };
-
+    function beforeUpload(file) {
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+        if (!isJpgOrPng) {
+          message.error('You can only upload JPG/PNG file!');
+        }
+        const isLt2M = file.size / 1024 / 1024 < 2;
+        if (!isLt2M) {
+          message.error('Image must smaller than 2MB!');
+        }
+        return isJpgOrPng && isLt2M;
+    }
     return(
         <>
             <Form form={form} name={"normal_login"}
@@ -76,19 +86,17 @@ const SignUpCommonent = () => {
                   onFinish={onFinish}
                   xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}
             >
-                {/* <Form.Item>
-                    <Upload
-                        name="avatar"
-                        listType="picture-card"
-                        className="avatar-uploader"
-                        showUploadList={false}
-                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                        beforeUpload={beforeUpload}
-                        onChange={this.handleChange}
-                    >
-                        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-                    </Upload>
-                </Form.Item> */}
+                <Upload
+                    name="avatar"
+                    listType="picture-card"
+                    className="avatar-uploader"
+                    showUploadList={false}
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    beforeUpload={beforeUpload}
+                    onChange={handleChange}
+                >
+                    {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                </Upload>
                 <Form.Item
                     name={"name"}
                     rules={
